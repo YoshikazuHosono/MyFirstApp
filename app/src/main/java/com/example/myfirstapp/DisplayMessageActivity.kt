4f37d66414051588bcf1432130dfc7202c7afa9f
YoshikazuHosono <yyhhwork@gmail.com>
2019-06-29 22:08:33 +0900

    メイン画面で入力した文字列を表示する画面を作成

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
@@ -2,11 +2,19 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.TextValueSanitizer
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val inputText = intent.getStringExtra(EXSTRA_MESSAGE)
+
+        val txtView = findViewById<TextView>(R.id.textView).apply{
+            text = inputText
+        }
     }
 }
